3986e28bb8326fe8581282ed77693f501dd7e1b6
Hambali Marcel <marcelinohambali@gmail.com>
2024-05-20 04:49:29 +0700

    feat: add views

--- materi/views.py
+++ materi/views.py
@@ -1,1 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Materi
+from .forms import MateriForm
+
+def materi_list(request):
+    materials = Materi.objects.all()
+    return render(request, 'materi_list.html', {'materials': materials})
+
+def upload_materi(request):
+    if request.method == 'POST':
+        form = MateriForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('materi_list')
+    else:
+        form = MateriForm()
+    return render(request, 'upload_materi.html', {'form': form})
